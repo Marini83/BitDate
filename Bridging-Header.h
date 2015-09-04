88cb3ef186424785ca53839e8e8698ece890a882
marini83 <kallashi@gmail.com>
2015-09-04 11:52:30 -0400

    Parse Facebook Username login

---
+++ Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  BitDate
+//
+//  Created by User  on 2015-09-02.
+//  Copyright (c) 2015 Wub.com. All rights reserved.
+//
+
+#ifndef BitDate_Bridging_Header_h
+#define BitDate_Bridging_Header_h
+
+
+#endif
